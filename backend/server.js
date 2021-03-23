3e6234ce13224ce2739c032e617d2a233712a53c
Palak <palakgupta2712@gmail.com>
2021-03-23 23:50:14 +0530

    MongoDB setup

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,29 @@
 const express = require('express')
 const cors = require('cors')
+//Import the mongoose module
+const mongoose = require('mongoose')
 
 require('dotenv').config()
 
 const app = express()
 const port = process.env.PORT || 3000
-
 app.use(cors())
 app.use(express.json())
+
+//Set up default mongoose connection
+const uri = 'mongodb://localhost:27017/myapp';
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true})
+
+//Get the default connection
+const db_connection = mongoose.connection;
+
+//Establish database connection
+db_connection.once('open', () =>{
+    console.log("MongoDB database connection established")
+})
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello MERN!')
 })
 
 app.listen(port, () => {
