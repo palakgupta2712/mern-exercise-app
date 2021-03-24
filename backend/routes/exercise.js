315dead5be440c25dac3a06ed1142b42ad922ed2
Palak <palakgupta2712@gmail.com>
2021-03-24 13:57:15 +0530

    added api endpoints for exercises route

--- backend/routes/exercise.js
+++ backend/routes/exercise.js
@@ -25,5 +25,38 @@ router.post('/add', function (req, res) {
     newExercise.save()
     .then(() => res.json('Exercise added!'))
     .catch(err => res.status(400).json('Error:' + err))
-})
+});
+
+//Get exercise by id
+router.get('/:id', function (req, res) {
+  Exercise.findById(req.params.id)
+    .then(exercise => res.json(exercise))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+//Delete exercise
+router.delete('/:id', function (req, res) {
+  Exercise.findByIdAndDelete(req.params.id)
+    .then(() => res.json('Exercise deleted.'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+//Update exercise
+router.post('/update/:id', function (req, res){
+  Exercise.findById(req.params.id)
+    .then(exercise => {
+      exercise.username = req.body.username;
+      exercise.description = req.body.description;
+      exercise.duration = Number(req.body.duration);
+      exercise.date = Date.parse(req.body.date);
+
+      exercise.save()
+        .then(() => res.json('Exercise updated!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+    })
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+
+
 module.exports = router;
